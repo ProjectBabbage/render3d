a6b098ace5a28b00c7d511ec501bc14f63a62245
Quentin Garchery <garchery.quentin@gmail.com>
2021-06-12 19:20:14 +0200

    SurfaceFromSurfaces

--- main.go
+++ main.go
@@ -13,9 +13,11 @@ func main() {
 	n := Vector{0, 0, 1}
 	t := Triangle{p1, p2, p3, n}
 
+	scene := SurfaceFromSurfaces([]Surface{t})
+
 	for i := Lx; i <= Hx; i++ {
 		for j := Ly; j <= Hy; j++ {
-			point := t.Intersect(Ray{Eye, Pxy(i, j)})
+			point := scene.Intersect(Ray{Eye, Pxy(i, j)})
 			fmt.Println(point)
 		}
 	}
--- util/surface.go
+++ util/surface.go
@@ -1,5 +1,6 @@
 package util
 
+// When hasIntersection, we should have distance >= 0
 type IntersectRes struct {
 	hasIntersection bool
 	distance        float64
@@ -12,8 +13,35 @@ type Surface interface {
 	Print()
 }
 
-type Scene = []Surface
+type scene struct {
+	support []Surface
+}
 
-// func (s Scene) Intersect(r Ray) IntersectRes {
+func (s scene) Intersect(r Ray) IntersectRes {
+	res := IntersectRes{false, 0, Vector{0, 0, 0}}
+	for _, surf := range s.support {
+		I := surf.Intersect(r)
+		if I.hasIntersection && (!res.hasIntersection || res.hasIntersection && I.distance < res.distance) {
+			res.hasIntersection = I.hasIntersection
+			res.distance = I.distance
+			res.position = I.position
+		}
+	}
+	return res
+}
 
-// }
+func (s scene) Translate(v Vector) {
+	for _, surf := range s.support {
+		surf.Translate(v)
+	}
+}
+
+func (s scene) Print() {
+	for _, surf := range s.support {
+		surf.Print()
+	}
+}
+
+func SurfaceFromSurfaces(surfs []Surface) Surface {
+	return scene{surfs}
+}
